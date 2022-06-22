6c061b189cf69207568fcdf7df1f81d6721ca495
bao.thai <bao.thai@asnet.com.vn>
2022-06-22 09:11:41 +0700

    Fix conditional rendering sample

--- react-examples/src/samples/conditional-rendering/index.tsx
+++ react-examples/src/samples/conditional-rendering/index.tsx
@@ -1,10 +1,23 @@
-import { Greeting } from "./GreetingUser";
-import { LoginButton, LogoutButton } from "./LogginBtn";
-export const LoginController = () => {
-  if(isLoggedIn) {
-    <Greeting isLoggedIn={true} />
-  }
-  return <Greeting isLoggedIn={false}/>
 
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+function MissedGoal() {
+	return <h1>MISSED!</h1>;
 }
 
+function MadeGoal() {
+	return <h1>GOAL!</h1>;
+}
+
+function Goal(props: any) {
+  const isGoal = props.isGoal;
+  if (isGoal) {
+    return <MadeGoal/>;
+  }
+  return <MissedGoal/>;
+}
+
+// @ts-expect-error
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<Goal isGoal={false} />);
