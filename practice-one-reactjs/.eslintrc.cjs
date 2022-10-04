150bb3d580b65494c702cf7e3d3349768149068d
Bao Gia Thai <bao.thai@asnet.com.vn>
2022-10-04 16:05:36 +0700

    Implement pre-commit

---
+++ practice-one-reactjs/.eslintrc.cjs
@@ -0,0 +1,33 @@
+module.exports = {
+  parser: '@typescript-eslint/parser', // Specifies the ESLint parser
+  parserOptions: {
+    ecmaVersion: 2020, // Allows for the parsing of modern ECMAScript features
+    sourceType: 'module', // Allows for the use of imports
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
+  ],
+  plugins: ['eslint-plugin-prettier', 'react', '@typescript-eslint', 'prettier'],
+  rules: {
+    'prettier/prettier': ['error', { endOfLine: 'auto' }, { usePrettierrc: true }],
+    'react/prop-types': 'off',
+    'react/react-in-jsx-scope': 'off',
+    'react/display-name': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/ban-ts-ignore': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-var-requires': 'off',
+    'react-hooks/exhaustive-deps': 'off',
+    '@typescript-eslint/no-non-null-assertion': 'off',
+    camelcase: [
+      'warn',
+      {
+        properties: 'never',
+        ignoreDestructuring: true,
+      },
+    ],
+  },
+};
---
+++ practice-one-reactjs/src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  const abc;
+  return (
+    <div className='App'>
+      <h1>Vite + React</h1>
+      <p className='read-the-docs'>Click on the Vite and React logos to learn more</p>
+    </div>
+  );
+}
+
+export default App;
