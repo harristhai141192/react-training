8967aa557612bc4250ee6e098a4d6703caec4274
Bao Gia Thai <bao.thai@asnet.com.vn>
2022-10-07 15:19:16 +0700

    feat: #22 delete html element, optimize coding styles

--- practice-one-reactjs/src/components/Footer/index.tsx
+++ practice-one-reactjs/src/components/Footer/index.tsx
@@ -7,12 +7,8 @@ import './index.styles.css';
 const Footer = () => {
   return (
     <div className='footer'>
-      <div className='footerLabel'>
-        <h1 className='footerLabelName'>Pokedex</h1>
-      </div>
-      <div className='footerDescription'>
-        <p>Copyright &#169; 2019</p>
-      </div>
+      <h1 className='footerLabel'>Pokedex</h1>
+      <p className='footerDescription'>Copyright &#169; 2019</p>
     </div>
   );
 };
