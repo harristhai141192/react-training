6d68132ea8ef7acca18168a14d1602367375064b
Bao Gia Thai <bao.thai@asnet.com.vn>
2022-10-07 16:37:08 +0700

    feat: #25 Implement card component

--- practice-one-reactjs/src/components/Card/index.stories.tsx
+++ practice-one-reactjs/src/components/Card/index.stories.tsx
@@ -1,1 +1,27 @@
-// Card StoryBook
+// Libraries
+import { ComponentMeta, ComponentStory } from '@storybook/react';
+
+// Components
+import Card from '.';
+
+// Images
+import { FireDragon } from '@assets/index';
+
+// CSS
+import '../../index.css';
+
+export default {
+  title: 'Practice-One/Card',
+  component: Card,
+} as ComponentMeta<typeof Card>;
+
+const Template: ComponentStory<typeof Card> = (args) => <Card {...args} />;
+
+export const Primary = Template.bind({});
+
+Primary.args = {
+  cardCode: '012',
+  photo: FireDragon,
+  name: 'Charmander',
+  description: 'Smiling Pokemon Charmander',
+};
