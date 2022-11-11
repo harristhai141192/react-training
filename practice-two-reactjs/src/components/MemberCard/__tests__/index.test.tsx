6ead80c3293c2df479069ee747ecec002c636108
Bao Gia Thai <bao.thai@asnet.com.vn>
2022-11-11 16:31:51 +0700

    feat: #75 add missing test

---
+++ practice-two-reactjs/src/components/MemberCard/__tests__/index.test.tsx
@@ -0,0 +1,20 @@
+// libraries
+import { render } from '@utils/testUtils';
+// components
+import MemberCard from '../index';
+import '@testing-library/jest-dom';
+import Button from '@components/Button';
+
+// theme
+
+describe('Component [MemberCard] should match snapshot', () => {
+  test('It should match snapshot', () => {
+    const { container } = render(
+      <MemberCard modalTitle={''} memberName={''}>
+        <Button />
+      </MemberCard>,
+    );
+
+    expect(container).toMatchSnapshot();
+  });
+});
