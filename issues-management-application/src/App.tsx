eae9ee066a48c43f2255e2f9a043d254581e3bc2
harristhai141192 <bao.thai@asnet.com.vn>
2022-12-08 14:08:32 +0700

    fix: delete unuse code and file

--- issues-management-application/src/App.tsx
+++ issues-management-application/src/App.tsx
@@ -1,19 +1,8 @@
-import CommentDetail from '@components/CommentDetail';
 import { ChakraProvider } from '@chakra-ui/react';
 import themes from './themes';
 
 function App() {
-  return (
-    <ChakraProvider theme={themes} resetCSS={true}>
-      <CommentDetail
-        userName='Bao Thai'
-        userCommentTime='15 minutes'
-        userComment='This is comment sample for storybook!!!!!!!!!!This is comment sample for storybook!!!!!!!!!!This is comment sample for storybook!!!!!!!!!!This is comment sample for storybook!!!!!!!!!!This is comment sample for storybook!!!!!!!!!!This is comment sample for storybook!!!!!!!!!!This is comment sample for storybook!!!!!!!!!!This is comment sample for storybook!!!!!!!!!!This is comment sample for storybook!!!!!!!!!!This is comment sample for storybook!!!!!!!!!!This is comment sample for storybook!!!!!!!!!!This is comment sample for storybook!!!!!!!!!!This is comment sample for storybook!!!!!!!!!!This is comment sample for storybook!!!!!!!!!!This is comment sample for storybook!!!!!!!!!!This is comment sample for storybook!!!!!!!!!!This is comment sample for storybook!!!!!!!!!!This is comment sample for storybook!!!!!!!!!!This is comment sample for storybook!!!!!!!!!!This is comment sample for storybook!!!!!!!!!!This is comment sample for storybook!!!!!!!!!!This is comment sample for storybook!!!!!!!!!!This is comment sample for storybook!!!!!!!!!!This is comment sample for storybook!!!!!!!!!!This is comment sample for storybook!!!!!!!!!!This is comment sample for storybook!!!!!!!!!!This is comment sample for storybook!!!!!!!!!!This is comment sample for storybook!!!!!!!!!!This is comment sample for storybook!!!!!!!!!!'
-        userImage='https://genk.mediacdn.vn/2018/6/28/photo-5-1530178537559354148061.jpg'
-        imageAlt=''
-      />
-    </ChakraProvider>
-  );
+  return <ChakraProvider theme={themes} resetCSS={true}></ChakraProvider>;
 }
 
 export default App;
