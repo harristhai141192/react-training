2773965f08ae37f5e574c0a6cd4b5fd5331b22e8
harristhai141192 <bao.thai@asnet.com.vn>
2022-12-09 10:31:25 +0700

    draft: feature bar

---
+++ issues-management-application/src/components/FeatureBar/index.tsx
@@ -0,0 +1,3 @@
+const FeatureBar = () => {};
+
+export default FeatureBar;
---
+++ issues-management-application/src/components/constants/feature.ts
@@ -0,0 +1,6 @@
+export enum FEATURES {
+  LOCK = 'Lock Conversation',
+  UNLOCK = 'Unlock Issue',
+  EDIT = 'Edit Issue',
+  DELETE = 'Delete Issue',
+}
